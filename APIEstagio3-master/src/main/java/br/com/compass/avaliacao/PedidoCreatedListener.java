17380d402cd624ae91044f3ed289135fe97fd5ef
marcelemontalvao <mmontalvaoblog@outlook.com>
2022-07-31 23:24:34 -0300

    Atualização do pom.xml e comentários de códigos

---
+++ APIEstagio3-master/src/main/java/br/com/compass/avaliacao/PedidoCreatedListener.java
@@ -0,0 +1,15 @@
+package br.com.compass.avaliacao;
+/*
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PedidoCreatedListener {
+    @RabbitListener(queues = "pedidos.v1.pedido-created.generate-pagamento")
+    public void onPedidoCreated(PedidoCreatedEvent event) {
+        System.out.println("Id recebido " + event.getId());
+        System.out.println("Valor recebido " + event.getTotal());
+    }
+
+}
+*/
